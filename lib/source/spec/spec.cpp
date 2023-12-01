593fd54e1637a701eabd452dc1aeae576775e34d
WerWolv <werwolv98@gmail.com>
2023-12-01 20:37:53 +0100

    spec: Add remaining 8051 opcodes

--- lib/source/spec/spec.cpp
+++ lib/source/spec/spec.cpp
@@ -4,46 +4,58 @@
 
 namespace disasm::spec {
 
+    namespace {
+
+        std::pair<std::string, size_t> formatOpcode(const std::vector<Opcode> &opcodes, std::span<const u8> bytes, bool allowMultiple = false) {
+            std::string result;
+            size_t advance = 0;
+            for (const Opcode &opcode : opcodes) {
+                if (auto formattedValue = opcode.format(bytes.subspan(advance)); formattedValue.has_value()) {
+                    result += formattedValue.value();
+                    result.append(" ");
+
+                    advance += opcode.getSize();
+
+                    if (!allowMultiple)
+                        break;
+                }
+            }
+
+            return { result, advance };
+        }
+
+    }
+
     std::vector<std::string> Spec::disassemble(const std::span<const u8> bytes) const {
         std::vector<std::string> disassembly;
 
         size_t offset = 0x00;
         std::string result;
         while (offset < bytes.size()) {
-            bool found = false;
-
-            for (const Opcode &prefix : getPrefixes()) {
-                if (auto formattedValue = prefix.format(bytes.subspan(offset)); formattedValue.has_value()) {
-                    result += formattedValue.value();
-                    result.append(" ");
-
-                    offset += prefix.getSize();
-                    found = true;
-                }
+            {
+                auto [formattedValue, advance] = formatOpcode(getPrefixes(), bytes.subspan(offset), true);
+                result += formattedValue;
+                offset += advance;
             }
 
-            for (const Opcode &opcode : getOpcodes()) {
-                if (auto formattedValue = opcode.format(bytes.subspan(offset)); formattedValue.has_value()) {
-                    result += formattedValue.value();
-                    offset += opcode.getSize();
-                    found = true;
-                    break;
-                }
-            }
-
-            if (!found) {
-                result.clear();
-                disassembly.emplace_back("???");
+            {
+                auto [formattedValue, advance] = formatOpcode(getOpcodes(), bytes.subspan(offset));
+                result += formattedValue;
                 offset += 1;
-            } else {
-                disassembly.emplace_back(std::move(result));
+
+                if (advance == 0) {
+                    result.clear();
+                    disassembly.emplace_back("???");
+                    offset += 1;
+                } else {
+                    disassembly.emplace_back(std::move(result));
+                }
             }
+
         }
 
         return disassembly;
     }
-
-
 
 }
 
